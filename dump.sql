bda3667402ff0f325689be30b48dc91e8a6dcac1
Rafo <rafael_dvid@hotmail.com>
2023-02-17 01:41:41 -0300

    criando pasta controladores, banco e tabelas

--- dump.sql
+++ dump.sql
@@ -1,1 +1,38 @@
---1
+create database dindin;
+create table usuarios(
+    id serial primary key,
+    nome text not null,
+    email text unique not null,
+    senha varchar(100) not null
+);
+create table categorias(
+    id serial primary key,
+    descricao text unique not null
+);
+create table transacoes(
+    id serial primary key,
+    descricao text not null,
+    valor int not null,
+    data date not null,
+    categoria_id int references categorias(id),
+    usuario_id int references usuarios(id),
+    tipo text not null
+);
+insert into categorias (descricao)
+values ('Alimentação'),
+    ('Assinaturas e Serviços'),
+    ('Casa'),
+    ('Mercado'),
+    ('Cuidados Pessoais'),
+    ('Educação'),
+    ('Família'),
+    ('Lazer'),
+    ('Pets'),
+    ('Presentes'),
+    ('Roupas'),
+    ('Saúde'),
+    ('Transporte'),
+    ('Salário'),
+    ('Vendas'),
+    ('Outras receitas'),
+    ('Outras despesas');
--- src/conexao.js
+++ src/conexao.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     port: 5432,
     user: "postgres",
     password: "postgres",
-    database: "0000"
+    database: "dindin"
 })
 
 module.exports = pool;
